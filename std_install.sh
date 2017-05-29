c55be0d6616b21e55d44d89b7df43b59c506ba13
R. Garcia-Dias <rafaelgd@iac.es>
2017-05-29 11:31:58 +0100

    std install

---
+++ std_install.sh
@@ -0,0 +1,30 @@
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo apt-get update
+sudo apt --fix-broken install
+sudo apt-get -y install python2
+sudo apt-get -y install python3
+sudo apt-get -y install ipython
+sudo apt-get -y install git
+sudo apt-get -y install latex
+sudo apt-get -y install sublime-text
+sudo apt-get -y install python-matplotlib
+sudo apt-get -y install python-numpy
+sudo apt-get -y install vim
+sudo apt-get -y install liferea
+sudo apt-get -y install htop
+sudo apt-get -y install vlc
+sudo apt-get install python-pip python-dev python-virtualenv 
+sudo pip install --upgrade pip
+sudo pip install astropy
+sudo pip install astroquery
+sudo pip install vaex
+sudo pip install h5py
+sudo pip install ipyvolume
+sudo pip install jupyter
+sudo pip install scikit-learn
+sudo pip install tensorflow
+sudo pip install scipy
+wget https://repo.continuum.io/archive/Anaconda2-4.3.1-Linux-x86_64.sh
+bash Anaconda2-4.3.1-Linux-x86_64.sh
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
