8e7ba9619c242d0aa0ea4523fa32bfd350e3defb
R. Garcia-Dias <rafaelgd@iac.es>
2017-11-12 16:28:01 +0000

    Better conf.

---
+++ modify_bash.sh
@@ -0,0 +1,15 @@
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\[\033[00m\]\[\033[01;34m\]\w\[\033[32m\]$(parse_git_branch)\[\033[00m\]$ '
+
+# added by Anaconda3 installer
+export PATH="/home/d/anaconda3/bin:$PATH"
+
+# added by Anaconda2 installer
+export PATH="/home/d/anaconda2/bin:$PATH"
+
+alias tarea='ssh -X rafaelgd@tarea'
+alias tareax='ssh -X rafaelgd@tarea.ll.iac.es'
+alias cdg='cd /media/d/Data/git/'
+alias flow='source /home/d/.apps/tensorFlow/bin/activate'
+alias gojournal='source /media/d/Data/git/journalclub/bin/activate'
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
--- std_install.sh
+++ std_install.sh
@@ -1,34 +1,38 @@
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo apt install ubuntu-restricted-extras
 sudo apt-get update
 sudo apt --fix-broken install
-sudo apt-get -y install python2
-sudo apt-get -y install python3
-sudo apt-get -y install ipython
 sudo apt-get -y install git
 sudo apt-get -y install latex
 sudo apt-get -y install texmaker
-sudo apt-get -y install sublime-text
-sudo apt-get -y install python-matplotlib
-sudo apt-get -y install python-numpy
 sudo apt-get -y install vim
-sudo apt-get -y install liferea
 sudo apt-get -y install htop
 sudo apt-get -y install vlc
-sudo apt-get install python-pip python-dev python-virtualenv 
-sudo pip install --upgrade pip
-sudo pip install astropy
-sudo pip install astroquery
-sudo pip install vaex
-sudo pip install h5py
-sudo pip install ipyvolume
-sudo pip install jupyter
-sudo pip install scikit-learn
-sudo pip install tensorflow
-sudo pip install scipy
-wget https://repo.continuum.io/archive/Anaconda2-4.3.1-Linux-x86_64.sh
-bash Anaconda2-4.3.1-Linux-x86_64.sh
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-sudo dpkg -i gitkraken-amd64.deb
 sudo apt-get -y install spell
-sudo apt install ubuntu-restricted-extras
-sudo pip install networkx
+sudo dpkg -i code_1.18.0-1510145176_amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo dpkg -i libcudnn7_7.0.3.11-1+cuda9.0_amd64.deb
+sudo dpkg -i mendeleydesktop_1.17.11-stable_amd64.deb
+sudo dpkg -i skypeforlinux-64.deb
+mkdir ~/.apps/
+cp -r Aladin/ ~/.apps/
+cp topcat-full.jar ~/.apps/
+cp rambox ~/.apps/
+chmod a+x rambox
+ln -s /usr/bin/rambox
+echo "alias topcat='java -jar ~/.apps/topcat-full.jar'" >> ~/.bashrc
+echo "alias aladin='java -jar ~/.apps/Aladin/Aladin.jar'" >> ~/.bashrc
+bash Anaconda2-5.0.1-Linux-x86_64.sh
+bash Anaconda3-5.0.1-Linux-x86_64.sh
+ln -s ~/.apps/anaconda2/bin/python2.7 /usr/bin/python2
+ln -s ~/.apps/anaconda2/bin/ipython /usr/bin/ipython2
+ln -s ~/.apps/anaconda2/bin/pip /usr/bin/pip2
+ln -s ~/.apps/anaconda2/bin/jupyter-notebook /usr/bin/jupyter2-notebook
+ln -s ~/.apps/anaconda2/bin/jupyter2 /usr/bin/jupyter2
+ln -s ~/.apps/anaconda3/bin/python /usr/bin/python
+ln -s ~/.apps/anaconda3/bin/python3.6 /usr/bin/python3
+ln -s ~/.apps/anaconda3/bin/ipython /usr/bin/ipython3
+ln -s ~/.apps/anaconda3/bin/pip /usr/bin/pip3
+ln -s ~/.apps/anaconda3/bin/jupyter-notebook /usr/bin/jupyter3-notebook
+ln -s ~/.apps/anaconda3/bin/jupyter3 /usr/bin/jupyter3
+sudo apt --fix-broken install
